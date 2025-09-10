25c5e4f1b715918e14fc7f8caf553f29ddece3c7
Prince <princekashishbbu@gmail.com>
2025-09-10 22:21:36 +0530

    working with auth

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,7 +6,27 @@ export const users = pgTable("users", {
   email: varchar("email").notNull().unique(),
   image: varchar("image"),
   provider: varchar("provider").notNull(),
-  oauth_id:text("oauthid").notNull(),
-  createdAt: timestamp("created_at").defaultNow(), 
-  updatedAt: timestamp("updated_at").defaultNow(), 
+  oauth_id: text("oauthid").notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
 });
+
+export const sessions = pgTable("sessions", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  user_id: uuid("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  token: text("token").notNull().unique(),
+  ip_addres: text("ip_address"),
+  user_agent: text("user_agent"),
+  createdAt: timestamp("created_at").defaultNow(),
+  expiredAt: timestamp("created_at"),
+});
+
+export const roles = pgTable("roles", {
+  id: uuid("id").notNull().defaultRandom(),
+  name: text("name"),
+  createdAt:timestamp("createdAt").defaultNow(),
+});
+
+
